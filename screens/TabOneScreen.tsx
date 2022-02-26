e2dfa6e62ca1bf931be452f05a2c96a9f3279fdf
Jim Porter <jim@parlio.io>
2022-02-25 21:32:48 -0600

    initial list of locations, need to handle pagination.

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,9 +1,8 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
 import { useState, useEffect } from 'react';
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
-import { Button, ThemeProvider } from 'react-native-elements';
+import { Button, ThemeProvider, ListItem } from 'react-native-elements';
 import {
   useQuery,
   gql
@@ -14,43 +13,37 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
       results{
         id name type dimension
         residents{
-          id name species
+          id name species image
         }
       }
     }
   }`
   const { loading, error, data } = useQuery(query);
-  if (loading) return <View style={styles.container}>
+  if (loading) return <View>
   <Text style={styles.title}>loading</Text></View>;
-  if (error) return <View style={styles.container}>
+  if (error) return <View>
   <Text style={styles.title}>Error</Text></View>;
-  console.log(data);
-  /*
-  */
   return (<ThemeProvider>
     <Button title="Hey!" />
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  </ThemeProvider>
+    <ScrollView>
+  {
+    data.locations.results.map((l:any, i:any) => (
+      <ListItem key={i} bottomDivider>
+        <ListItem.Content>
+          <ListItem.Title>{l.name}</ListItem.Title>
+          <ListItem.Subtitle>{l.type}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    ))
+  }
+    </ScrollView>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  }
 });
