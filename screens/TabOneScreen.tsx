00a2adbf19d157b8fee7660ab8c81ddfd50e09ba
Jim Porter <jim@parlio.io>
2022-02-25 22:44:39 -0600

    using a flatlist and apollo pagination for locations with in memory cache

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, FlatList } from 'react-native';
 import { useState, useEffect } from 'react';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
@@ -8,8 +8,8 @@ import {
   gql
 } from "@apollo/client";
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  const query = gql`{
-    locations{
+  const query = gql`query GetLocations($page:Int){
+    locations(page:$page){
       results{
         id name type dimension
         residents{
@@ -18,25 +18,35 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
       }
     }
   }`
-  const { loading, error, data } = useQuery(query);
+  const renderLocation = ({item} :{item:any}) => (
+    <ListItem key={item.id} bottomDivider>
+      <ListItem.Content>
+        <ListItem.Title>{item.id} - {item.name}</ListItem.Title>
+        <ListItem.Subtitle>{item.type}</ListItem.Subtitle>
+      </ListItem.Content>
+    </ListItem>
+  )
+  const [page,setPage] = useState(1)
+  const { loading, error, data, fetchMore } = useQuery(query,{variables:{page}});
   if (loading) return <View>
   <Text style={styles.title}>loading</Text></View>;
   if (error) return <View>
   <Text style={styles.title}>Error</Text></View>;
   return (<ThemeProvider>
-    <Button title="Hey!" />
-    <ScrollView>
-  {
-    data.locations.results.map((l:any, i:any) => (
-      <ListItem key={i} bottomDivider>
-        <ListItem.Content>
-          <ListItem.Title>{l.name}</ListItem.Title>
-          <ListItem.Subtitle>{l.type}</ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
-    ))
-  }
-    </ScrollView>
+    <Button title={page.toString()} />
+    <Text>{page}</Text>
+    <FlatList data={data.locations.results} renderItem={renderLocation} onEndReached={()=>{
+      fetchMore({
+        variables: {
+          page: page+1
+        },
+      })
+      setPage(page+1);
+    }}
+    onEndReachedThreshold={.5}
+    >
+
+    </FlatList>
     </ThemeProvider>
   );
 }
