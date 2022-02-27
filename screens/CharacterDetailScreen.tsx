5bbd29ec8a04adaefa425ea5b293e7e26b5bd187
Jim Porter <jim@parlio.io>
2022-02-27 17:05:59 -0600

    have successful app connection to backend through ngrok and query data returning.

--- screens/CharacterDetailScreen.tsx
+++ screens/CharacterDetailScreen.tsx
@@ -1,6 +1,26 @@
-import { StyleSheet, FlatList } from 'react-native';
+import { StyleSheet, FlatList, View } from 'react-native';
 import { RootTabScreenProps } from '../types';
 import { ThemeProvider, ListItem, Image, Text } from 'react-native-elements';
+import { ApolloClient, InMemoryCache, gql, useQuery } from '@apollo/client';
+const client = new ApolloClient({
+  uri: 'https://2fdc-216-62-160-73.ngrok.io/graphql',
+  cache: new InMemoryCache()
+});
+const getNotesQuery = gql`query getNotes($userId: ID!, $characterId: String!) {
+  getMyNotes(userID: $userId,characterID: $characterId) {
+    characterID body userID
+  }
+}`
+const updateNoteQuery = gql`mutation UpdateNote($input: NoteInput!) {
+  updateNote(input: $input) {
+    characterID body
+  }
+}`
+const createNoteQuery = gql`mutation CreateNote($input: NoteInput!) {
+  createNote(input: $input) {
+    characterID body
+  }
+}`
 const styles = StyleSheet.create({
     list: {
       width: '100%',
@@ -19,26 +39,35 @@ const renderEpisode = ({item} :{item:any}) => (
     </ListItem>
 )
 export default function CharacterDetailScreen({ route, navigation }: RootTabScreenProps<'TabOne'>) {
-    let data = route.params;
+    let character = route.params;
     console.log(route.params)
-    navigation.setOptions({title:data.name})
+    navigation.setOptions({title:character.name})
+    const { loading, error, data, fetchMore } = useQuery(getNotesQuery,{variables:{"userId": "1234mongo","characterId":"34"},'client':client});
+    if (loading) return <View>
+    <Text style={styles.title}>loading</Text></View>;
+    if (error){
+      console.log(error)
+      return <View>
+    <Text style={styles.title}>Error</Text></View>;
+    } 
+    console.log(data)
     return (<ThemeProvider>
         <ListItem.Content>
 
         <Image
-            source={{ uri: data.image }}
+            source={{ uri: character.image }}
             containerStyle={styles.item}
             />
         </ListItem.Content>
 
-        <Text h3>Species: {data.species}</Text>
-        <Text h3>Gender: {data.gender}</Text>
-        <Text h3>Status: {data.status}</Text>
+        <Text h3>Species: {character.species}</Text>
+        <Text h3>Gender: {character.gender}</Text>
+        <Text h3>Status: {character.status}</Text>
         <Text h3>Episodes:</Text>
         <FlatList
       keyExtractor={(location) => location.id.toString()}
       initialNumToRender={20}
-      data={data.episode}
+      data={character.episode}
       renderItem={renderEpisode}
       ></FlatList>
     </ThemeProvider>);
