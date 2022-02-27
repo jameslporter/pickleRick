3ad815786e669b56ced5900953447d4d03090ae1
Jim Porter <jim@parlio.io>
2022-02-27 17:04:36 -0600

    mutations implemented and query params updated for both userID and characterID.

--- server/index.js
+++ server/index.js
@@ -10,18 +10,41 @@ const typeDefs = gql`
     body: String
     userID: String
   }
+  input NoteInput {
+    characterID: ID!
+    body: String!
+    userID: String!
+  }
   type Query {
-    getMyNotes(userID: ID!): [Note]
+    getMyNotes(userID: ID!,characterID: String!): [Note]
+  }
+  type Mutation{
+    createNote(input: NoteInput!): Note
+    updateNote(input: NoteInput!): Note
   }
 `;
 
 const resolvers = {
     Query: {
       async getMyNotes(parent, args, context, info){
-          return await Note.find({userID:args.userID})
-          //return([{characterID:1,body:"a big giant note",userID:args.userID}])
+          return await Note.find({userID:args.userID,characterID:args.characterID})
       },
     },
+    Mutation: {
+        createNote(parent, args, context, info){
+            return Note.create({characterID:args.input.characterID,body:args.input.body,userID:args.input.userID})
+        },
+        updateNote(parent, args, context, info){
+            Note.findOneAndUpdate(
+                {userID:args.input.userID,characterID:args.input.characterID},
+                {characterID:args.input.characterID,body:args.input.body,userID:args.input.userID},
+                {new:true},
+                (err,doc,res)=>{
+                    return doc
+                }
+            );
+        }
+    }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
