c3120c31ca73d9e7480eba5d6fda7ae0a50a8699
Jim Porter <jim@parlio.io>
2022-02-27 22:48:46 -0600

    using uuid and async local storage to create a user ID. Details now loads the note. Need to add input field and create/update logic in next.

--- screens/CharacterDetailScreen.tsx
+++ screens/CharacterDetailScreen.tsx
@@ -1,7 +1,18 @@
 import { StyleSheet, FlatList, View } from 'react-native';
+import { useState } from 'react';
 import { RootTabScreenProps } from '../types';
 import { ThemeProvider, ListItem, Image, Text } from 'react-native-elements';
 import { ApolloClient, InMemoryCache, gql, useQuery } from '@apollo/client';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { v1 as uuidv1 } from 'uuid';
+
+const storeData = async (key:string,value:string) => {
+  try {
+    await AsyncStorage.setItem(key, value)
+  } catch (e) {
+    console.log(e)
+  }
+}
 const client = new ApolloClient({
   uri: 'https://2fdc-216-62-160-73.ngrok.io/graphql',
   cache: new InMemoryCache()
@@ -39,17 +50,32 @@ const renderEpisode = ({item} :{item:any}) => (
     </ListItem>
 )
 export default function CharacterDetailScreen({ route, navigation }: RootTabScreenProps<'TabOne'>) {
+    const [getID, setID] = useState('');
+    const getData = (key:string) => {
+      AsyncStorage.getItem(key).then((value)=>{
+        console.log('async value',value)
+        if(value != null){
+          setID(value);
+        }else{
+          let newID = uuidv1();
+          setID(newID)
+          storeData(key,newID)
+        }
+      })
+    }
+    getData('uniqueID')
     let character = route.params;
     console.log(route.params)
     navigation.setOptions({title:character.name})
-    const { loading, error, data, fetchMore } = useQuery(getNotesQuery,{variables:{"userId": "1234mongo","characterId":"34"},'client':client});
+    console.log(getID,character.id);
+    const { loading, error, data, fetchMore } = useQuery(getNotesQuery,{variables:{"userId": getID,"characterId":character.id},'client':client});
     if (loading) return <View>
     <Text style={styles.title}>loading</Text></View>;
     if (error){
       console.log(error)
       return <View>
     <Text style={styles.title}>Error</Text></View>;
-    } 
+    }
     console.log(data)
     return (<ThemeProvider>
         <ListItem.Content>
@@ -70,5 +96,12 @@ export default function CharacterDetailScreen({ route, navigation }: RootTabScre
       data={character.episode}
       renderItem={renderEpisode}
       ></FlatList>
+      { data.getMyNotes.length > 0 ?
+      <View>
+        <Text h3>Notes:</Text>
+        <Text h4>{data.getMyNotes[0].body}</Text>
+      </View>
+      : <View><Text>No note.</Text></View>}
+
     </ThemeProvider>);
 }
